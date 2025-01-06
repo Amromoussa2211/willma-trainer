2cee4318fc0b6b94c1cb6a7028b980d6ede07e4e
Amr <46786529+Amromoussa2211@users.noreply.github.com>
2025-01-06 14:11:13 +0200

    Update wdio.ci.conf.js

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -1,8 +1,8 @@
 const ciConfig = {
     capabilities: [{
         platformName: "Android",
-        "appium:deviceName": "emulator-5554",
-        "appium:udid": "emulator-5554",
+        "appium:deviceName": process.env.DEVICE_NAME || "emulator-5554",
+        "appium:udid": process.env.DEVICE_NAME || "emulator-5554",
         "appium:app": './apps/app-release-v15-0.3.2-2024-12-25-18-13.apk',
         "appium:appPackage": "com.willma.client.staging",
         "appium:appActivity": "com.willma.client.staging.MainActivity",
@@ -12,8 +12,6 @@ const ciConfig = {
         "appium:ignoreHiddenApiPolicyError": true,
         "appium:noReset": true,
         "appium:fullReset": false,
-        "appium:skipDeviceInitialization": true,
-        "appium:skipServerInstallation": true,
         "appium:skipUnlock": true
     }]
 };
