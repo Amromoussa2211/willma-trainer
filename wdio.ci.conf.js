0f3d4f70cb0dcf68f2797b69e705f79ab2061587
Amr <46786529+Amromoussa2211@users.noreply.github.com>
2025-01-06 14:35:35 +0200

    Update wdio.ci.conf.js

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -1,18 +1,14 @@
 const ciConfig = {
+    user: process.env.BROWSERSTACK_USERNAME,
+    key: process.env.BROWSERSTACK_ACCESS_KEY,
     capabilities: [{
         platformName: "Android",
-        "appium:deviceName": process.env.DEVICE_NAME || "emulator-5554",
-        "appium:udid": process.env.DEVICE_NAME || "emulator-5554",
-        "appium:app": './apps/app-release-v15-0.3.2-2024-12-25-18-13.apk',
-        "appium:appPackage": "com.willma.client.staging",
-        "appium:appActivity": "com.willma.client.staging.MainActivity",
+        "appium:deviceName": "Google Pixel 5",
+        "appium:app": process.env.BROWSERSTACK_APP_ID,
         "appium:automationName": "UiAutomator2",
         "appium:newCommandTimeout": 120,
         "appium:autoGrantPermissions": true,
-        "appium:ignoreHiddenApiPolicyError": true,
-        "appium:noReset": true,
-        "appium:fullReset": false,
-        "appium:skipUnlock": true
+        "appium:noReset": true
     }]
 };
 
