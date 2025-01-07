b40ad93e2edd5de08d15c076e9b27fa460d76eda
Amr Moussa <amro_kaza@hotmail.com>
2025-01-07 16:18:20 +0200

    addtray

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -3,7 +3,7 @@ const ciConfig = {
         platformName: "Android",
         "appium:deviceName": "emulator-5554",
         "appium:udid": "emulator-5554",
-        "appium:app": "./apps/app-release.apk", // Match APK path
+        "appium:app": "./apps/app-release.apk", // Ensure correct APK path
         "appium:appPackage": "com.willma.client.staging",
         "appium:appActivity": "com.willma.client.staging.MainActivity",
         "appium:automationName": "UiAutomator2",
@@ -18,4 +18,12 @@ const ciConfig = {
     }]
 };
 
-exports.config = { ...require('./wdio.conf.js').config, ...ciConfig };
+exports.config = {
+    ...require('./wdio.conf.js').config,
+    ...ciConfig,
+    hostname: 'localhost', // Ensure the Appium server hostname
+    port: 4723,            // Ensure the Appium server port
+    path: '/',             // Use "/" for Appium 2.0
+    connectionRetryTimeout: 90000,
+    connectionRetryCount: 3
+};
