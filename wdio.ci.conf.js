c8c6f03d8b53398ff276452fc37412d4d23a5987
Amr <46786529+Amromoussa2211@users.noreply.github.com>
2025-01-07 13:13:44 +0200

    Update wdio.ci.conf.js

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -3,7 +3,7 @@ const ciConfig = {
         platformName: "Android",
         "appium:deviceName": "emulator-5554",
         "appium:udid": "emulator-5554",
-        "appium:app": './apps/app-release.apk',  // Updated to match the downloaded APK path
+        "appium:app": './apps/app-release-v15-0.3.2-2024-12-25-18-13.apk',  // Updated to match the downloaded APK path
         "appium:appPackage": "com.willma.client.staging",
         "appium:appActivity": "com.willma.client.staging.MainActivity",
         "appium:automationName": "UiAutomator2",
