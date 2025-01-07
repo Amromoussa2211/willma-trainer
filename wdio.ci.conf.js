9ec628f6fb41faebd065bf11336a36428f0199dd
Amr <46786529+Amromoussa2211@users.noreply.github.com>
2025-01-07 14:02:02 +0200

    Update wdio.ci.conf.js

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -3,17 +3,17 @@ const ciConfig = {
         platformName: "Android",
         "appium:deviceName": "emulator-5554",
         "appium:udid": "emulator-5554",
-        "appium:app": './apps/app-release-v15-0.3.2-2024-12-25-18-13.apk',  // Updated to match the downloaded APK path
+        "appium:app": "/home/runner/work/client/client/apps/app-release-v15-0.3.2-2024-12-25-18-13.apk",  // Absolute path
         "appium:appPackage": "com.willma.client.staging",
         "appium:appActivity": "com.willma.client.staging.MainActivity",
         "appium:automationName": "UiAutomator2",
-        "appium:newCommandTimeout": 120,
+        "appium:newCommandTimeout": 300,  // Increased timeout
         "appium:autoGrantPermissions": true,
         "appium:ignoreHiddenApiPolicyError": true,
         "appium:noReset": true,
         "appium:fullReset": false,
-        "appium:skipDeviceInitialization": true,
-        "appium:skipServerInstallation": true,
+        "appium:skipDeviceInitialization": false,  // Disabled
+        "appium:skipServerInstallation": false,  // Disabled
         "appium:skipUnlock": true
     }]
 };
