1a1c74b984edfd55828b241585cca5732e37baa2
Amr Moussa <amro_kaza@hotmail.com>
2025-01-08 09:21:09 +0200

    SSSSSS

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -2,12 +2,13 @@ const ciConfig = {
     capabilities: [{
         platformName: "Android",
         "appium:deviceName": "emulator-5554",
-        "appium:udid": "emulator-5554",
+        // Removing the static UDID for emulator
+        // "appium:udid": "emulator-5554", 
         "appium:app": "./apps/app-release-v15-0.3.2-2024-12-25-18-13.apk",
         "appium:appPackage": "com.willma.client.staging",
         "appium:appActivity": "com.willma.client.staging.MainActivity",
         "appium:automationName": "UiAutomator2",
-        "appium:newCommandTimeout": 600,
+        "appium:newCommandTimeout": 600, // Increase timeout
         "appium:autoGrantPermissions": true,
         "appium:ignoreHiddenApiPolicyError": true,
         "appium:noReset": true,
@@ -16,9 +17,9 @@ const ciConfig = {
         "appium:skipServerInstallation": true,
         "appium:skipUnlock": false
     }],
-    waitforTimeout: 60000,
+    waitforTimeout: 60000, // Increase global timeout
     connectionRetryTimeout: 90000,
-    connectionRetryCount: 3,
+    connectionRetryCount: 3
 };
 
 exports.config = {
