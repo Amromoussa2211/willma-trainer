0a2634000de162be5542ce519a32c51e5da0e762
Amr Moussa <amro_kaza@hotmail.com>
2025-01-09 23:59:10 +0200

    yarab

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -46,75 +46,19 @@
 
 
 
-const localConfig = {
-  runner: 'local',
-  hostname: 'localhost', // Appium is running locally in the CI environment
-  protocol: 'http',
-  port: 4723, // Default Appium port
-  path: '/', // Default Appium path for Appium 2.x
-  specs: ['./test/specs/**/*.js'], // Path to your test specs
-  exclude: [],
-  maxInstances: 1, // Only one instance since we're using a single emulator
-  logLevel: 'info',
-  bail: 0,
-  waitforTimeout: 15000,
-  connectionRetryTimeout: 200000,
-  connectionRetryCount: 2,
-  framework: 'mocha',
-  reporters: [
-    'spec',
-    ['allure', {
-      outputDir: 'allure-results',
-      disableWebdriverStepsReporting: false,
-      disableWebdriverScreenshotsReporting: false,
-    }],
-  ],
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 120000,
+capabilities: [
+  {
+    platformName: 'Android',
+    'appium:deviceName': 'emulator-5554',
+    'appium:platformVersion': '13', // Update this to match the emulator's Android version
+    'appium:app': process.env.APK_PATH,
+    'appium:appPackage': 'com.willma.client.staging',
+    'appium:appActivity': 'com.willma.client.staging.MainActivity',
+    'appium:automationName': 'UiAutomator2',
+    'appium:newCommandTimeout': 120,
+    'appium:autoGrantPermissions': true,
+    'appium:noReset': true,
+    'appium:fullReset': false,
   },
-
-  services: [
-    ['appium', {
-      command: 'appium', // Use the globally installed Appium
-      args: {
-        relaxedSecurity: true, // Allow certain Appium features
-      },
-    }],
-  ],
-
-  capabilities: [
-    {
-      platformName: 'Android',
-      'appium:deviceName': 'emulator-5554', // Use the emulator device name
-      'appium:platformVersion': '12.0', // Match the emulator's Android version
-      'appium:app': process.env.APK_PATH, // Use the APK path from the environment variable
-      'appium:appPackage': 'com.willma.client.staging',
-      'appium:appActivity': 'com.willma.client.staging.MainActivity',
-      'appium:automationName': 'UiAutomator2',
-      'appium:newCommandTimeout': 120,
-      'appium:autoGrantPermissions': true,
-      'appium:noReset': true,
-      'appium:fullReset': false,
-    },
-  ],
-
-  onPrepare: async function (config, capabilities) {
-    console.log('Skipping device check and APK installation...');
-    // Add any other setup logic here
-  },
-
-  afterTest: async function (test, context, { error }) {
-    const screenshotDir = path.join(__dirname, 'screenshots');
-    if (!fs.existsSync(screenshotDir)) {
-      fs.mkdirSync(screenshotDir, { recursive: true });
-    }
-    if (error) {
-      const fileName = path.join(screenshotDir, `${test.title.replace(/[^a-zA-Z0-9]/g, '_')}_FAILED.png`);
-      await browser.saveScreenshot(fileName);
-      await allure.addAttachment('Failed Screenshot', fs.readFileSync(fileName), 'image/png');
-    }
-  },
-};
-
-exports.config = { ...require('./wdio.conf.js').config, ...localConfig };
+],
+ exports.config = { ...require('./wdio.conf.js').config, ...localConfig };
