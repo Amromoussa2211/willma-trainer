eec7bfa694fe782a38226ce98f45a98e410dc2e3
Amr Moussa <Amr.ibrahim@willma.life>
2025-05-19 18:41:00 +0300

    sync2

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -1,30 +1,30 @@
 const baseConfig = require('./wdio.conf.js');
 
 const ciConfig = {
-    capabilities: [{
-        platformName: 'Android',
-        'appium:automationName': 'UiAutomator2',
-        'appium:deviceName': 'Pixel_5_API_31',
-        'appium:platformVersion': '31.0',
-        "appium:appPackage": "com.willma.staging",
-        "appium:appActivity": "com.willma.staging.MainActivity",
-        'appium:app': process.env.APK_PATH,
-        'appium:noReset': false,
-        'appium:fullReset': true,
-        'appium:autoGrantPermissions': true,
-    }],
-    reporters: [
-        'spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverScreenshotsReporting: false,
-        }]
-    ],
-    logLevel: 'info',
-    bail: 0,
-    waitforTimeout: 45000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
+  capabilities: [{
+    platformName: 'Android',
+    'appium:automationName': 'UiAutomator2',
+    'appium:deviceName': 'Pixel_5_API_31',
+    'appium:platformVersion': '31.0',
+    "appium:appPackage": "com.willma.staging",
+    "appium:appActivity": "com.willma.staging.MainActivity",
+    'appium:app': './apps/app-release-trainer.apk',
+    'appium:noReset': false,
+    'appium:fullReset': true,
+    'appium:autoGrantPermissions': true,
+  }],
+  reporters: [
+    'spec',
+    ['allure', {
+      outputDir: 'allure-results',
+      disableWebdriverScreenshotsReporting: false,
+    }]
+  ],
+  logLevel: 'info',
+  bail: 0,
+  waitforTimeout: 45000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
 };
 
 exports.config = { ...baseConfig.config, ...ciConfig };
