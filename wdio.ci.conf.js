9764495285086e7be764aae27cf2e2b4126c71e9
Amr Moussa <Amr.ibrahim@willma.life>
2025-05-22 12:54:49 +0300

    full rest false

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -10,7 +10,7 @@ const ciConfig = {
     "appium:appActivity": "com.willma.staging.MainActivity",
     'appium:app': process.env.apk_CI_PATH ,
     'appium:noReset': false,
-    'appium:fullReset': true,
+    'appium:fullReset': false,
     'appium:autoGrantPermissions': true,
     'appium:newCommandTimeout': 600,
     'appium:androidDeviceReadyTimeout': 1200,
