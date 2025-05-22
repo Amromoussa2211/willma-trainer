8fb46e3bcf2bbcc0cf7f3a9965360d771d55516b
Amr Moussa <Amr.ibrahim@willma.life>
2025-05-22 13:03:02 +0300

    AAA

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -9,7 +9,7 @@ const ciConfig = {
     "appium:appPackage": "com.willma.staging",
     "appium:appActivity": "com.willma.staging.MainActivity",
     'appium:app': process.env.apk_CI_PATH ,
-    'appium:noReset': false,
+    'appium:noReset': true,
     'appium:fullReset': false,
     'appium:autoGrantPermissions': true,
     'appium:newCommandTimeout': 600,
