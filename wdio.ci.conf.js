56194aec82de67506bf8c7a7d18abddfc7c21831
Amro Moussa <Amr.ibrahim@willma.life>
2025-07-22 10:57:37 +0300

    Update wdio.ci.conf.js
    change config wedio ci

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -2,45 +2,29 @@ const baseConfig = require('./wdio.conf.js');
 const { execSync } = require('child_process');
 
 const ciConfig = {
+  specs: ['./test/specs/**/*.js'],
+  maxInstances: 1,
   capabilities: [{
     platformName: 'Android',
     'appium:automationName': 'UiAutomator2',
     'appium:deviceName': 'emulator-5554',
     'appium:platformVersion': '14',
-    "appium:appPackage": "com.willma.staging",
-    "appium:appActivity": "com.willma.staging.MainActivity",
-    'appium:app': process.env.apk_CI_PATH ,
-    },
-    {
-      platformName: 'Android',
-      'appium:automationName': 'UiAutomator2',
-      'appium:deviceName': 'emulator-5554',
-      'appium:platformVersion': '14',
-      "appium:appPackage": "com.client.app",
-      "appium:appActivity": "com.client.app.MainActivity",
-      'appium:app': process.env.appclient_path,
-      'appium:noReset': false,
-      'appium:fullReset': true,
-      'appium:autoGrantPermissions': true,
-      'appium:newCommandTimeout': 1800,
-      'appium:androidDeviceReadyTimeout': 1200,
-      'appium:avdLaunchTimeout': 1200000,
-      'appium:avdReadyTimeout': 1200000,
+    'appium:appPackage': 'com.willma.staging',
+    'appium:appActivity': 'com.willma.staging.MainActivity',
+    'appium:app': process.env.apk_CI_PATH,
     'appium:noReset': false,
     'appium:fullReset': true,
     'appium:autoGrantPermissions': true,
     'appium:newCommandTimeout': 1800,
     'appium:androidDeviceReadyTimeout': 1200,
-    'appium:avdLaunchTimeout': 1200000,
-    'appium:avdReadyTimeout': 1200000,
+    'appium:avdLaunchTimeout': 300000,
+    'appium:avdReadyTimeout': 300000,
   }],
-  before: async function (capabilities, specs) {
+  before: async function (capabilities) {
     try {
-      // Clear app data (cache) before starting activity
-      execSync(
-        `adb -s emulator-5554 shell pm clear com.willma.staging`,
-        { stdio: 'inherit' }
-      );
+      const device = capabilities['appium:deviceName'];
+      const pkg = capabilities['appium:appPackage'];
+      execSync(`adb -s ${device} shell pm clear ${pkg}`, { stdio: 'inherit' });
     } catch (err) {
       console.error('Failed to clear app cache:', err);
     }
@@ -58,8 +42,3 @@ const ciConfig = {
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3,
 };
-
-exports.config = { ...baseConfig.config, ...ciConfig };
-
-
-
