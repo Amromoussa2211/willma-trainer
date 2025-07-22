db7c135e9d5d6207beacb21a06e1d045f0c23f00
Amro Moussa <Amr.ibrahim@willma.life>
2025-07-22 12:50:03 +0300

    Update wdio.ci.conf.js
    add capabilty

--- wdio.ci.conf.js
+++ wdio.ci.conf.js
@@ -7,6 +7,7 @@ const ciConfig = {
     'appium:automationName': 'UiAutomator2',
     'appium:deviceName': 'emulator-5554',
     'appium:platformVersion': '14',
+    'appium:disableSuppressAccessibilityService': true,
     "appium:appPackage": "com.willma.staging",
     "appium:appActivity": "com.willma.staging.MainActivity",
     'appium:app': process.env.apk_CI_PATH,
